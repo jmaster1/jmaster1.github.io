4329f01719e5020d37cebf54f3dde34118de507b
timur <timur.kozlov@gmail.com>
2023-10-02 21:58:13 +0300

    sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,24 @@
+<?php
+$to = "timur.kozlov@gmail.com"; // info@pvl.ee
+$subject = {$_POST['subject'];
+
+$message = "
+<html>
+<head>
+<title>This is a test HTML email</title>
+</head>
+<body>
+<p>Test email. Please ignore.</p>
+</body>
+</html>
+";
+
+// It is mandatory to set the content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers. From is required, rest other headers are optional
+$headers .= 'From: <info@example.com>' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>
