e27828b798685e583159f26b66d1b29e5dbaf9d7
timur.kozlov <timur.kozlov@gmail.com>
2023-10-12 16:50:18 +0300

    sendmail

--- sendmail.php
+++ sendmail.php
@@ -1,47 +1,38 @@
 <?php
 // https://www.tutorialspoint.com/php/php_sending_emails.htm
-function mail_attachment ($from , $to, $subject, $message, $attachment){
-     $fileatt = $attachment; // Path to the file
-     $fileatt_type = "application/octet-stream"; // File Type
-
-     $start = strrpos($attachment, '/') == -1 ?
-        strrpos($attachment, '//') : strrpos($attachment, '/')+1;
-
-     $fileatt_name = substr($attachment, $start, strlen($attachment)); // Filename that will be used for the file as the attachment
-
-     $email_from = $from; // Who the email is from
-     $subject = "New Attachment Message";
-
-     $email_subject =  $subject; // The Subject of the email
-     $email_txt = $message; // Message that the email has in it
-     $email_to = $to; // Who the email is to
-
-     $headers = "From: ".$email_from;
-     $file = fopen($fileatt,'rb');
-     $data = fread($file,filesize($fileatt));
+function mail_attachment ($from , $to, $subject, $message, $attachmentFile, $attachmentName) {
+     $headers =
+     $file = fopen($attachmentFile, 'rb');
+     $data = fread($file, filesize($attachmentFile));
+     $data = chunk_split(base64_encode($data));
      fclose($file);
 
-     $msg_txt="\n\n You have recieved a new attachment message from $from";
      $semi_rand = md5(time());
      $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-     $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . "
-        boundary=\"{$mime_boundary}\"";
-
-     $email_txt .= $msg_txt;
+     $headers = "From: ".$from .
+        "\nMIME-Version: 1.0" .
+        "\nContent-Type: multipart/mixed;" .
+        "\n boundary=\"{$mime_boundary}\"";
 
      $email_message .= "This is a multi-part message in MIME format.\n\n" .
-        "--{$mime_boundary}\n" . "Content-Type:text/html;
-        charset = \"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" .
-        $email_txt . "\n\n";
+        "--{$mime_boundary}\n" .
+        "Content-Type:text/html;charset= \"UTF-8\"\n" .
+        "Content-Transfer-Encoding: 7bit\n\n" .
+        $message . "\n\n";
 
-     $data = chunk_split(base64_encode($data));
+     $email_message .=
+        "--{$mime_boundary}\n" .
+        "Content-Type: application/octet-stream; name = \"{$fileatt_name}\"\n" .
+        "Content-Disposition: attachment;\n" .
+        " filename = \"{$fileatt_name}\"\n" .
+        "Content-Transfer-Encoding: base64\n\n" .
+        $data .
+        "\n\n" .
+        "--{$mime_boundary}--\n";
 
-     $email_message .= "--{$mime_boundary}\n" .
-        "Content-Type: {$fileatt_type};\n" . " name = \"{$fileatt_name}\"\n" .
-        "Content-Disposition: attachment;\n" . " filename = \"{$fileatt_name}\"\n" .
-        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n" . "--{$mime_boundary}--\n";
-
-     $ok = mail($email_to, $email_subject, $email_message, $headers);
+        echo "\nemail_message=" . $email_message;
+        echo "\nheaders=" . $headers;
+     $ok = mail($to, $subject, $email_message, $headers);
 
      if($ok) {
         echo "File Sent Successfully.";
@@ -50,29 +41,39 @@ function mail_attachment ($from , $to, $subject, $message, $attachment){
         die("Sorry but the email could not be sent. Please go back and try again!");
      }
 }
-
+echo "<pre>";
 $to = "timur.kozlov@gmail.com"; // info@pvl.ee
+$from = $_REQUEST['email'];
 $subject = $_REQUEST['subject'];
 $message = $_REQUEST['message'];
 $file = $_FILES["file"];
-
+echo "\nto=" . $to;
+echo "\nfrom=" . $from;
+echo "\nsubject=" . $subject;
+echo "\nmessage=" . $message;
+echo "\nfile=" . $file;
 if($file) {
-    move_uploaded_file($_FILES["file"]["tmp_name"], 'temp/'.basename($_FILES['file']['name']));
-    mail_attachment($from, $to, $subject, $message, ("temp/".$_FILES["file"]["name"]));
+    $filePath = $file['tmp_name'];
+    $fileName = $file['name'];
+    $toName = 'temp/'.basename($name);
+    echo "\nfilePath=" . $filePath;
+    echo "\nfileName=" . $fileName;
+    mail_attachment($from, $to, $subject, $message, $filePath, $fileName);
 } else {
     $header = "From:" . $_POST['email'] . "\r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n";
 
     $retval = mail($to, $subject, $message, $header);
-    echo " | to=" . $to;
-    echo " | subject=" . $subject;
-    echo " | message=" . $message;
-    echo " | header=" . $header;
+    echo "\nto=" . $to;
+    echo "\nsubject=" . $subject;
+    echo "\nmessage=" . $message;
+    echo "\nheader=" . $header;
     if( $retval == true ) {
         echo "Message sent successfully...";
     } else {
         echo "Message could not be sent...";
     }
 }
+echo "</pre>";
 ?>
