c56ed7a374ee1a9e8475a6f6ade6a7e76ae0f5b7
timur <timur.kozlov@gmail.com>
2023-10-15 20:08:36 +0300

    sendmail

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,6 @@
 <?php
 // https://www.tutorialspoint.com/php/php_sending_emails.htm
 function mail_attachment ($from , $to, $subject, $message, $attachmentFile, $attachmentName) {
-     $headers =
-     $file = fopen($attachmentFile, 'rb');
-     $data = fread($file, filesize($attachmentFile));
-     $data = chunk_split(base64_encode($data));
-     fclose($file);
-
      $semi_rand = md5(time());
      $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
      $headers = "From: ".$from .
@@ -18,34 +12,44 @@ function mail_attachment ($from , $to, $subject, $message, $attachmentFile, $att
         "--{$mime_boundary}\n" .
         "Content-Type:text/html;charset= \"UTF-8\"\n" .
         "Content-Transfer-Encoding: 7bit\n\n" .
-        $message . "\n\n";
+        $message . "\n\n" .
+        "--{$mime_boundary}\n";
 
-     $email_message .=
-        "--{$mime_boundary}\n" .
-        "Content-Type: application/octet-stream; name = \"{$fileatt_name}\"\n" .
-        "Content-Disposition: attachment;\n" .
-        " filename = \"{$fileatt_name}\"\n" .
-        "Content-Transfer-Encoding: base64\n\n" .
-        $data .
-        "\n\n" .
-        "--{$mime_boundary}--\n";
-
-        echo "\nemail_message=" . $email_message;
-        echo "\nheaders=" . $headers;
-     $ok = mail($to, $subject, $email_message, $headers);
-
-     if($ok) {
+    if($attachmentFile) {
+         $file = fopen($attachmentFile, 'rb');
+         $data = fread($file, filesize($attachmentFile));
+         $data = chunk_split(base64_encode($data));
+         fclose($file);
+         unlink($attachmentFile);
+         $email_message .=
+             "Content-Type: application/octet-stream; name = \"{$attachmentName}\"\n" .
+             "Content-Disposition: attachment;\n" .
+             " filename = \"{$attachmentName}\"\n" .
+             "Content-Transfer-Encoding: base64\n\n" .
+             $data .
+             "\n\n" .
+             "--{$mime_boundary}--\n";
+    }
+    echo "\nemail_message=" . $email_message;
+    echo "\nheaders=" . $headers;
+    $ok = mail($to, $subject, $email_message, $headers);
+    if($ok) {
         echo "File Sent Successfully.";
-        unlink($attachment); // delete a file after attachment sent.
-     } else {
+    } else {
         die("Sorry but the email could not be sent. Please go back and try again!");
-     }
+    }
 }
 echo "<pre>";
 $to = "timur.kozlov@gmail.com"; // info@pvl.ee
 $from = $_REQUEST['email'];
+$name = $_REQUEST['name'];
+$phone = $_REQUEST['phone'];
 $subject = $_REQUEST['subject'];
-$message = $_REQUEST['message'];
+$message = "name: " . $name . "<br>" .
+    "phone: " . $phone . "<br>" .
+    "message:<br><pre>" .
+    $_REQUEST['message'] .
+    "</pre>";
 $file = $_FILES["file"];
 echo "\nto=" . $to;
 echo "\nfrom=" . $from;
@@ -58,22 +62,7 @@ if($file) {
     $toName = 'temp/'.basename($name);
     echo "\nfilePath=" . $filePath;
     echo "\nfileName=" . $fileName;
-    mail_attachment($from, $to, $subject, $message, $filePath, $fileName);
-} else {
-    $header = "From:" . $_POST['email'] . "\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-
-    $retval = mail($to, $subject, $message, $header);
-    echo "\nto=" . $to;
-    echo "\nsubject=" . $subject;
-    echo "\nmessage=" . $message;
-    echo "\nheader=" . $header;
-    if( $retval == true ) {
-        echo "Message sent successfully...";
-    } else {
-        echo "Message could not be sent...";
-    }
 }
+mail_attachment($from, $to, $subject, $message, $filePath, $fileName);
 echo "</pre>";
 ?>
